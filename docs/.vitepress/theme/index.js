0ae16f2114049ff9983bebf7253b9cf64915d804
Meriç <merichrocks@gmail.com>
2026-07-30 00:18:59 +0300

    Add people portraits and fix Veyran record

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -7,12 +7,13 @@ import PeopleHistoryReference from './components/PeopleHistoryReference.vue'
 import PeopleCultureReference from './components/PeopleCultureReference.vue'
 import StateRelations from './components/StateRelations.vue'
 import CapitalLife from './components/CapitalLife.vue'
+import PeoplePortrait from './components/PeoplePortrait.vue'
 import EraFooter from './components/EraFooter.vue'
 
 export default {
   extends: DefaultTheme,
   Layout: () => h(DefaultTheme.Layout, null, {
-      'doc-after': () => [h(PeopleHistoryReference), h(PeopleCultureReference), h(StateRelations), h(CapitalLife), h(EraFooter)]
+      'doc-after': () => [h(PeoplePortrait), h(PeopleHistoryReference), h(PeopleCultureReference), h(StateRelations), h(CapitalLife), h(EraFooter)]
   }),
   enhanceApp({ app }) {
     app.component('PeopleGallery', PeopleGallery)
