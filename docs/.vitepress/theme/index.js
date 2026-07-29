4fe04a91307e0f136d7da83b1fc010514ae92027
Meriç <merichrocks@gmail.com>
2026-07-30 00:00:22 +0300

    Add state political and trade records

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -5,12 +5,13 @@ import PeopleGallery from './components/PeopleGallery.vue'
 import FaithCatalogue from './components/FaithCatalogue.vue'
 import PeopleHistoryReference from './components/PeopleHistoryReference.vue'
 import PeopleCultureReference from './components/PeopleCultureReference.vue'
+import StateRelations from './components/StateRelations.vue'
 import EraFooter from './components/EraFooter.vue'
 
 export default {
   extends: DefaultTheme,
   Layout: () => h(DefaultTheme.Layout, null, {
-      'doc-after': () => [h(PeopleHistoryReference), h(PeopleCultureReference), h(EraFooter)]
+      'doc-after': () => [h(PeopleHistoryReference), h(PeopleCultureReference), h(StateRelations), h(EraFooter)]
   }),
   enhanceApp({ app }) {
     app.component('PeopleGallery', PeopleGallery)
